c13cbca80198d5621231da9912f70748e26632ef
abhi <abhigtm3003@gmail.com>
2025-06-15 00:20:33 +0530

    implemented the same using test cases

--- Patterns/pb1.py
+++ Patterns/pb1.py
@@ -1,8 +1,14 @@
-def sol():
-   a = "*"
-   for i in range(0,4):
-    for j in range(0,4):
-        print(a,end="")
-    print("\n")
+def sol(a):
+    for i in range(0,a):
+     for j in range(0,a):
+        print("*",end="")
+     print("\n")
 
-sol()
+
+
+
+b = int(input("Enter the test cases you want :- "))
+for i in range (0,b):
+   a = int(input("Enter the numbers you want to print these patterns :- "))
+   sol(a)
+
